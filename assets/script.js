bcf941bd907dc8606e2769aaecaa0aa4482d64d1
Jared de Uriarte <jared.deuri@gmail.com>
2021-03-16 21:46:29 -0400

    trying to get data from json

--- assets/script.js
+++ assets/script.js
@@ -1,76 +1,50 @@
-
-
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-// "https://api.openweathermap.org/data/2.5/weather?q=" {city%20name} &appid=b38f2321fa2666ca5f377e831d6efe20
-
-// https://api.openweathermap.org/data/2.5/weather?q=london&appid=b38f2321fa2666ca5f377e831d6efe20
-
-
- // On click, take the value in the input and 1) store it in localStorage and 2) feed it into API call and 3) fetch API
-
-
 // Go Button Event Listener
 $('.goBtn').on('click', function () {
     // Grabbing the value from .searchedCity input
-    var searchedCity = $(".searchedCity").val();
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity.replaceAll(" ", "") + "&appid=b38f2321fa2666ca5f377e831d6efe20"
-    console.log(url)
-   
-   
+    var cityName = $(".cityName").val();
+    // Concatinating searchedCity with api to create url
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.replaceAll(" ", "") + "&units=imperial&appid=b38f2321fa2666ca5f377e831d6efe20"
+    // console.log(url)
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].main)
 
+                // var userName = document.createElement('h3');
+                // var issueTitle = document.createElement('h4');
+                // var issueBody = document.createElement('p');
+                // userName.textContent = data[i].user.login;
+                // issueTitle.textContent = data[i].title;
+                // issueBody.textContent = data[i].body;
+                // issueContainer.append(userName);
+                // issueContainer.append(issueTitle);
+                // issueContainer.append(issueBody);
+            }
+        });
 
-
-    
 })
 
 
 
 
 
-// var comment = $(this).siblings(".description").val()
-    // var hour = $(this).siblings(".hour").text()
-    // localStorage.setItem(hour, comment);
 
 
 
 
-
-
-
-
-
-
-// api.openweathermap.org/data/2.5/weather?q={city name}
-
-
-// api.openweathermap.org/data/2.5/weather?q=london&appid=b38f2321fa2666ca5f377e831d6efe20
 
 
 // var issueContainer = document.getElementById('issues');
 // var fetchButton = document.getElementById('fetch-button');
 
 // function getApi() {
-//   var requestUrl = 'https://api.github.com/repos/IBM/clai/issues?per_page=5';
+//     var requestUrl = 'https://api.github.com/repos/IBM/clai/issues?per_page=5';
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for (var i = 0; i < data.length; i++) {
-//         var userName = document.createElement('h3');
-//         var issueTitle = document.createElement('h4');
-//         var issueBody = document.createElement('p');
-//         userName.textContent = data[i].user.login;
-//         issueTitle.textContent = data[i].title;
-//         issueBody.textContent = data[i].body;
-//         issueContainer.append(userName);
-//         issueContainer.append(issueTitle);
-//         issueContainer.append(issueBody);
-//       }
-//     });
+
 // }
 // fetchButton.addEventListener('click', getApi);
 
